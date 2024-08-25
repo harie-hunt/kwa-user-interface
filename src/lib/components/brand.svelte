9b1b8a668107c4586149f11772d75cfeccced970
momo-hunt <momohunt8@gmail.com>
2024-08-25 14:33:38 +0700

    create-sidebar

--- src/lib/components/brand.svelte
+++ src/lib/components/brand.svelte
@@ -2,7 +2,7 @@
 	import { assets } from '$app/paths';
 </script>
 
-<div class="mx-3 flex items-center gap-2 font-semibold">
+<div class="mx-3 flex items-center gap-2 text-lg font-semibold">
 	<img src="{assets}/fav_koperasi.png" alt="Logo Koperasi" class="h-5 w-5" />
 	<h1 class="block lg:hidden">KWA</h1>
 	<h1 class="hidden lg:block">Koperasi Wanita Anggrek</h1>
--- src/routes/(admin)/+layout.svelte
+++ src/routes/(admin)/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Navbar from '$lib/components/navbar.svelte';
+	import Sidebar from '$lib/components/sidebar.svelte';
 	import type { LayoutData } from './$types';
 
 	// export let data: LayoutData;
@@ -9,6 +10,12 @@
 	<title>Koperasi Wanita Anggrek</title>
 </svelte:head>
 
-<Navbar />
+<div class="grid h-dvh grid-cols-[max-content_1fr] grid-rows-[max-content_1fr]">
+	<Navbar class="col-span-full" />
 
-<slot />
+	<Sidebar class="" />
+
+	<main class="p-3">
+		<slot />
+	</main>
+</div>
