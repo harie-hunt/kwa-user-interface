38891dee6e6370761a68d4e6b21783238235843b
momo-hunt <momohunt8@gmail.com>
2024-08-25 14:09:56 +0700

    profil

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import Brand from './brand.svelte';
+	import ProfilUser from './profil-user.svelte';
 </script>
 
 <div class="flex h-9 items-center bg-primary-500 text-white">
 	<Brand />
+
+	<ProfilUser />
 </div>
